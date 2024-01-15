6f2f0b89333234c73721806a53f42e0d1df1565a
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-01-15 15:00:03 +0100

    add non local connection

--- config/database.js
+++ config/database.js
@@ -1,7 +1,14 @@
 var mongoose = require('mongoose');
+const localDB =  (db = "db0") => `mongodb://localhost/${db}`;
+const atlasDB = (db = "db0") => `mongodb+srv://jonatanvahlberg:123123abcabcadmin@node30.hdzx1ml.mongodb.net/${db}?retryWrites=true&w=majority;`;
 
 async function connectToDB(db = "db0") {
-    await mongoose.connect('mongodb://localhost/' + db, { useNewUrlParser: true, useUnifiedTopology: true });
+    let url = atlasDB(db);
+    if(process.env.NODE_ENV === 'development') {
+        url = localDB(db);
+    }
+    console.log("url: ", url, process.env.NODE_ENV);
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
 
